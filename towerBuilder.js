74dfdf24d6fbbd99a1021497ab88944c061ec885
Greg Nicotera <42983801+gregnicotera@users.noreply.github.com>
2022-05-09 19:51:37 -0400

    Completed Tower Builder kata

---
+++ towerBuilder.js
@@ -0,0 +1,8 @@
+function towerBuilder(nFloors) {
+  let arr = []
+  for(let i = 0; i < nFloors; i++){
+      arr[i] = ' '.repeat(nFloors - i - 1) + '*'.repeat(2 * i + 1) + ' '.repeat(nFloors - i - 1)
+  }
+  console.log(arr)
+  return arr
+}
