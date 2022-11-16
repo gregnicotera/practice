b64ebc5436eb984c517dc30bd7520e9afa2fed53
Greg Nicotera <gregorynicotera@gmail.com>
2022-11-16 18:21:03 -0500

    Started Palindrome Leetcode Easy

---
+++ LeetCode/Easy/isPalindrome.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x - can be large numbers
+ * @return {boolean} - if x is palindrome
+ */
+var isPalindrome = function(x) {
+     x = x.toString().split(``);
+	 for(let i = 0; i < x.length / 2){
+		 
+	 }
+};
